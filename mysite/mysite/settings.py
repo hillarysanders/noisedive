3c81dc915b41d290df350e37447f265b58e5ba4c
Hillary Sanders <hillary.sanders@invincea.com>
2021-06-08 15:28:58 -0700

    media?

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = ['127.0.0.1']
