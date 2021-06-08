cadaf35a75396d19d63b6b0a2bb5b23f61c5c2b7
Hillary Sanders <hillary.sanders@invincea.com>
2021-06-08 13:44:08 -0700

    new env secret key

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,9 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR,'templates')]
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1rali6n^e(7ti2h(bzj0zo0p%#zx8a4h7p2&&$f4w1n(1l(3d)'
+# SECRET_KEY = 'django-insecure-1rali6n^e(7ti2h(bzj0zo0p%#zx8a4h7p2&&$f4w1n(1l(3d)'
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
