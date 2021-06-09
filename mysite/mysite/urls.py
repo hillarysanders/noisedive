c597e05296640e2ba9fb846e6b770d8f412fbfb1
Hillary Sanders <hillary.sanders@invincea.com>
2021-06-09 12:54:42 -0700

    1

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -30,8 +30,8 @@ if settings.DEBUG:
 else:
 #     pass
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     urlpatterns += re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT})
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += re_path(r'^media/(?P<path>.*)$', servey,{'document_root': settings.MEDIA_ROOT})
 #     urlpatterns += re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 
 
