0786bff5782b0f115b7e7d73262e13e87d450120
Hillary Sanders <hillary.sanders@invincea.com>
2021-06-11 12:16:29 -0700

    moved templates, also added custom css

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,8 +5,11 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'blog/index.html'
 
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+def index(request):
+    return render(request, 'blog/index.html', dict())
